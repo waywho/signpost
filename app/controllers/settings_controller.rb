d3ca2df07cad6c1fab12f0aa226965253077b9ad
WeiHsi Hu <walzerfan@yahoo.com>
2026-05-25 13:36:04 +0200

    refactor: simplify SettingsController with declarative config
    Whitelist settings keys per section, auto-cast integers/floats,
    shared save_settings/save_encrypted helpers. Removed 6 repetitive
    private methods.

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -1,4 +1,16 @@
 class SettingsController < ApplicationController
+  SETTINGS = {
+    "github"      => %w[github_username],
+    "slack"       => %w[slack_capture_mode slack_brain_emoji poll_interval_minutes],
+    "search"      => %w[search_default_threshold],
+    "compression" => %w[compression_after_months reanalysis_message_threshold reanalysis_quiet_minutes]
+  }.freeze
+
+  INTEGER_SETTINGS = %w[poll_interval_minutes compression_after_months reanalysis_message_threshold reanalysis_quiet_minutes].freeze
+  FLOAT_SETTINGS = %w[search_default_threshold].freeze
+
+  ENCRYPTED_KEYS = %w[openai_api_key github_pat slack_bot_token slack_user_token slack_app_token].freeze
+
   def show
     @watched_channels = WatchedChannel.order(:channel_name)
     @noise_filters = NoiseFilter.order(:category)
@@ -8,91 +20,60 @@ class SettingsController < ApplicationController
     case params[:section]
     when "ai_backend"
       Setting.set("global", "ai_backend", params[:ai_backend]) if params[:ai_backend].in?(ClaudeService::BACKENDS)
-      EncryptedSetting.set("credentials", "anthropic_api_key", params[:anthropic_api_key]) if params[:anthropic_api_key].present?
+      save_encrypted(:anthropic_api_key)
     when "api_keys"
-      update_api_keys
+      ENCRYPTED_KEYS.each { |key| save_encrypted(key) }
+    when "github", "slack", "search", "compression"
+      save_settings(params[:section])
     when "github"
-      update_github
-    when "slack"
-      update_slack
-    when "search"
-      update_search
-    when "compression"
-      update_compression
+      save_github_repos
     when "watched_channel_add"
-      add_watched_channel
+      WatchedChannel.create!(channel_id: params[:channel_id], channel_name: params[:channel_name], capture_mode: params[:capture_mode])
     when "watched_channel_remove"
-      remove_watched_channel
+      WatchedChannel.find_by(channel_id: params[:channel_id])&.destroy
     when "noise_filter_add"
-      add_noise_filter
+      NoiseFilter.create!(category: params[:filter_category], description: params[:filter_description])
     when "noise_filter_remove"
-      remove_noise_filter
+      NoiseFilter.find(params[:filter_id])&.destroy
     when "calendar"
-      update_calendar
+      save_encrypted(:google_ical_url)
     end
 
     redirect_to settings_path, notice: "Settings saved."
+  rescue ActiveRecord::RecordInvalid => e
+    redirect_to settings_path, alert: e.message
   end
 
   private
 
-  def update_api_keys
-    %w[openai_api_key anthropic_api_key github_pat slack_bot_token slack_user_token slack_app_token].each do |key|
-      value = params[key]
-      EncryptedSetting.set("credentials", key, value) if value.present?
+  def save_settings(section)
+    SETTINGS.fetch(section, []).each do |key|
+      next unless params[key].present?
+      value = cast_value(key, params[key])
+      Setting.set("global", key, value)
     end
+
+    save_github_repos if section == "github"
   end
 
-  def update_github
-    Setting.set("global", "github_username", params[:github_username]) if params[:github_username].present?
-    if params[:github_repos].present?
-      repos = params[:github_repos].split(",").map(&:strip).reject(&:blank?)
-      Setting.set("global", "github_repos", repos)
+  def save_encrypted(key)
+    value = params[key.to_s]
+    EncryptedSetting.set("credentials", key.to_s, value) if value.present?
+  end
+
+  def save_github_repos
+    return unless params[:github_repos].present?
+    repos = params[:github_repos].split(",").map(&:strip).reject(&:blank?)
+    Setting.set("global", "github_repos", repos)
+  end
+
+  def cast_value(key, value)
+    if key.in?(INTEGER_SETTINGS)
+      value.to_i
+    elsif key.in?(FLOAT_SETTINGS)
+      value.to_f
+    else
+      value
     end
-  end
-
-  def update_slack
-    Setting.set("global", "slack_capture_mode", params[:slack_capture_mode]) if params[:slack_capture_mode].present?
-    Setting.set("global", "slack_brain_emoji", params[:slack_brain_emoji]) if params[:slack_brain_emoji].present?
-    Setting.set("global", "poll_interval_minutes", params[:poll_interval_minutes].to_i) if params[:poll_interval_minutes].present?
-  end
-
-  def update_search
-    Setting.set("global", "search_default_threshold", params[:search_default_threshold].to_f) if params[:search_default_threshold].present?
-  end
-
-  def update_compression
-    Setting.set("global", "compression_after_months", params[:compression_after_months].to_i) if params[:compression_after_months].present?
-    Setting.set("global", "reanalysis_message_threshold", params[:reanalysis_message_threshold].to_i) if params[:reanalysis_message_threshold].present?
-    Setting.set("global", "reanalysis_quiet_minutes", params[:reanalysis_quiet_minutes].to_i) if params[:reanalysis_quiet_minutes].present?
-  end
-
-  def add_watched_channel
-    WatchedChannel.create!(
-      channel_id: params[:channel_id],
-      channel_name: params[:channel_name],
-      capture_mode: params[:capture_mode]
-    )
-  rescue ActiveRecord::RecordInvalid => e
-    redirect_to settings_path, alert: e.message and return
-  end
-
-  def remove_watched_channel
-    WatchedChannel.find_by(channel_id: params[:channel_id])&.destroy
-  end
-
-  def add_noise_filter
-    NoiseFilter.create!(category: params[:filter_category], description: params[:filter_description])
-  rescue ActiveRecord::RecordInvalid => e
-    redirect_to settings_path, alert: e.message and return
-  end
-
-  def remove_noise_filter
-    NoiseFilter.find(params[:filter_id])&.destroy
-  end
-
-  def update_calendar
-    value = params[:google_ical_url]
-    EncryptedSetting.set("credentials", "google_ical_url", value) if value.present?
   end
 end
