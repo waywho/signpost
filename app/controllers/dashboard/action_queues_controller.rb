f74ccd1ee50fe8ad6729fd324cef9f1ab80acd22
WeiHsi Hu <walzerfan@yahoo.com>
2026-06-03 21:20:15 +0200

    Restyle and remove ignored

--- app/controllers/dashboard/action_queues_controller.rb
+++ app/controllers/dashboard/action_queues_controller.rb
@@ -4,8 +4,6 @@ class Dashboard::ActionQueuesController < ApplicationController
     @actionable_items = base.actionable.by_priority
     @discussion_items = base.discussions.by_priority
     @acknowledge_items = base.acknowledgements.by_priority
-    @ignored_count = ActionItem.ignored.count
-    @ignored_items = ActionItem.ignored.includes(:slack_topic).by_priority if params[:show_ignored]
     @developers_for_select = Developer.by_name
     @github_repos = Setting.get("global", "github_repos", default: []) || []
   end
